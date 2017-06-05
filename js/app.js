40b46d977f6776fbfc293d8f9b4bd5b9561d32d0
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2017-06-05 16:50:46 -0700

    board changed to 3x3 due to difficulty completing the 4x4

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // ------ VARIABLES ------ //
 var boxes = $('.gameboard div')
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ' ']
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, ' ']
 var emptyBox;
 var currentNumber;
