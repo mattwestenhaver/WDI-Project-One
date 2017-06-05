a75d847c4eb042e8c9237c72784095d645657e4a
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2017-06-05 09:53:09 -0700

    added JS styles to empty box

--- js/app.js
+++ js/app.js
@@ -1,13 +1,47 @@
 // ------ VARIABLES ------ //
 var boxes = $('.gameboard div')
-
-
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ' ']
+var emptyBox;
 
 // ------ FUNCTIONS ------ //
+
+// adds event listeners to all the boxes
 for (var i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener('click', boxClickHandler)
 }
 
+// randomize the numbers array
+function shuffle(numbers) {
+  var currentIndex = numbers.length, temporaryValue, randomIndex;
+  while (0 !==currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = numbers[currentIndex];
+    numbers[currentIndex] = numbers[randomIndex];
+    numbers[randomIndex] = temporaryValue;
+  }
+  return numbers;
+}
+numbers = shuffle(numbers);
+
+// adds numbers to each div
+$.each( numbers, function( i, val ) {
+  $( ".gameboard div" ).eq(i).text(val);
+});
+
+// find the empty div
+emptyBox = $(".tile:contains(' ')").css('background-color', 'maroon').css('border-color', 'black')
+
+// empty = $('.class').filter(function() {
+//   if ($(this).text() === " ") {
+//     empty = $(".class:contains(' ')").css('background-color', 'blue')
+//   }
+// })
+
+
+// controls what happens when each box is clicked
 function boxClickHandler() {
-  console.log(boxes[i] + " has been clicked")
+  console.log("box has been clicked")
+  
 }
